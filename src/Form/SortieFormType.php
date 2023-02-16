c11d72cacd5d356aa028b6699bebe459b4e95cf3
mordeInfo <jonathansin27@gmail.com>
2023-02-16 14:10:42 +0100

    création de sortie : TO DO imbriquer la ville dans le formulaire de création.
    création AFFICHAGE d'une sortie :
    route pour l'affichage d'une sortie crée ainsi que le twig( reste css a faire)
    
    js

--- src/Form/SortieFormType.php
+++ src/Form/SortieFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Etat;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -37,8 +38,13 @@ class SortieFormType extends AbstractType
 //            ->add('organisteur',EntityType::class,
 //               ["class" => Lieu::class, "choice_label" => "nom"])
          //   ->add('site')
+            //->add('ville',EntityType::class,["class"=> Ville::class,"choice_label"=>"nom"])
             ->add('lieu', EntityType::class,
                 ["class" => Lieu::class, "choice_label" => "nom"])
+
+//            ->add('ville', EntityType::class, [
+//                'class' => 'App\Entity\Ville',"choice_label"=>"nom"])
+
             ->add('Enregistrer', SubmitType::class, ['attr' => ['value' => 'Enregistrer']])
             ->add('Publier', SubmitType::class, ['attr' => ['value' => 'Publier']])
             ->add('Annuler', SubmitType::class, ['attr' => ['value' => 'Annuler']])
