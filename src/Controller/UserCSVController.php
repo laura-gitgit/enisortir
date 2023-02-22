3c985e4b3f30a43fef371344c919af59b8861474
Lowra <laura.boudard@gmail.com>
2023-02-22 15:03:05 +0100

    intégration fichier CSV fonctionnel

--- src/Controller/UserCSVController.php
+++ src/Controller/UserCSVController.php
@@ -11,12 +11,17 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserCSVController extends AbstractController
 {
     #[Route('/user', name: '_fichierCSV')]
-    public function fichierCSV(Request $request, EntityManagerInterface $em, SiteRepository $siteRepository): Response
+    public function fichierCSV(
+        Request $request,
+        EntityManagerInterface $em,
+        SiteRepository $siteRepository,
+        UserPasswordHasherInterface $passwordHasher): Response
     {
         $userCSVForm = $this->createForm(UserCSVType::class);
         $userCSVForm->handleRequest($request);
@@ -44,16 +49,19 @@ class UserCSVController extends AbstractController
                 $newUser->setNom($utilisateur['C']);
                 $newUser->setPrenom($utilisateur['D']);
                 $newUser->setTelephone($utilisateur['E']);
-                $newUser->setPassword($utilisateur['B']);
+
                 $newUser->setActif(true);
                 $newUser->setAdministrateur(false);
                 $newUser->setUpdatedAt(new \DateTime('now'));
                 $newUser->setRoles(['ROLE_USER']);
                 $newUser->setPseudo(' ');
+
+                $mdpHache = $passwordHasher->hashPassword($newUser, $utilisateur['B']);
+                $newUser->setPassword($mdpHache);
+
                 $users->add($newUser);
             }
 
-            dd($users);
             if($userCSVForm->isValid()){
                 try {
                     foreach ($users as $user) {
